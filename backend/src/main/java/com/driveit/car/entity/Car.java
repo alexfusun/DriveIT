b75eee39f99d8be7ca86b81e7fae046d35bf1897
Alex Fu <alexfusun@gmail.com>
2026-06-14 21:07:03 +0200

    Car entity

---
+++ backend/src/main/java/com/driveit/car/entity/Car.java
@@ -0,0 +1,67 @@
+package com.driveit.car.entity;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.driveit.brand.entity.Brand;
+import com.driveit.brand.entity.CarModel;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cars")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Car {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+
+    @ManyToOne
+    @JoinColumn(name = "model_id")
+    private CarModel carModel;
+
+    @Column(nullable = false)
+    private String version;
+
+    @Column(nullable = false)
+    private int year;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private BigDecimal averageRating = BigDecimal.ZERO;
+
+    @Column(nullable = false)
+    private int reviewCount = 0;
+
+    @OneToOne(mappedBy = "car", cascade = CascadeType.ALL)
+    private CarSpec spec;
+
+    @OneToMany(mappedBy = "car", cascade = CascadeType.ALL)
+    private List<CarImage> images = new ArrayList<>();
+
+}
